65d37e545461b328885366213ca29ca74144de7d
Pedro Henrique <phlima378@gmail.com>
2025-04-25 12:46:20 -0300

    arquivo appcolors comentado

--- lib/ui/_core/app_colors.dart
+++ lib/ui/_core/app_colors.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+
+// essa classe é onde defini as cores que utilizei no meu projeto
 abstract class AppColors {
 static const Color backgroundColor = Color(0xFFFFF1E6);
 static const Color mainColor = Color(0xFF61c2a2);
