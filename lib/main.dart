51a60576db501980d8c75a1f74dc8930920bb124
Pedro Henrique <phlima378@gmail.com>
2025-04-25 12:48:46 -0300

    arquivo main comentado

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:app_desafio/ui/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
-
+// Cria um observador de rotas, que permite monitorar as mudanças de navegação (útil para saber quando uma tela foi empurrada, removida, etc.).Usado na função de _loadConfeitarias
 final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
 void main() {
@@ -17,6 +17,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: const HomeScreen(),
       navigatorObservers: [routeObserver],
+      // Adiciona o routeObserver como observador das rotas de navegação.
+      // Isso é útil para detectar eventos como didPush, didPop etc.
     );
   }
 }
